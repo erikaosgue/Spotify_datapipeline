8dcd22f96594475c8451ddb5fadc19bbf505bfa7
erikaosgue <1476@holbertonschool.com>
2022-02-12 13:38:40 -0500

    Program that makes the request of the spotify playlist

--- kafka-python/src/get_playlist_from_spotify.py
+++ kafka-python/src/get_playlist_from_spotify.py
@@ -1,33 +1,52 @@
 """
 Get playlist data from a scpefic user of spotify
 """
-
+import random
+from sp_producer import producer_func
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
+import time
+import signal
+import sys
 
 load_dotenv()
-CLIENT_ID=os.environ["client_id"]
-SECRET_ID=os.environ.get("client_secret")
-
+CLIENT_ID=os.environ["CLIENT_ID"]
+SECRET_ID=os.environ.get("SECRET_ID")
 
 auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=SECRET_ID)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 def get_playlist(user='spotify'):
-
+    """
+    Get the playlist from a specific user 
+    """
     playlists = sp.user_playlists(user)
     while playlists:
         for i, playlist in enumerate(playlists['items']):
-            print("%4d playlist_uri: %s playlist_name: %s" % (i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
+            playlist_dict = {
+                'playlists_offset': i + 1 + playlists['offset'],
+                'playlist_name': playlist['name'],
+                'playlist_uri':  playlist['uri']
+                }
+            producer_func(playlist_dict)
+            time_to_sleep = random.randint(1, 11)
+            time.sleep(time_to_sleep)
         if playlists['next']:
             playlists = sp.next(playlists)
         else:
             playlists = None
 
+def signal_handler(sig, frame):
+    """
+    Handler to catch the CRTL+C keyword to stop the program
+    """
+    print('\n= BYE! =')
+    sys.exit(0)
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler) 
     get_playlist()
