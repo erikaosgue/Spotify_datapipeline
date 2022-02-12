c0cfcd04319c9ef34596694dc41f89a06272dcd1
erikaosgue <1476@holbertonschool.com>
2022-02-12 13:35:19 -0500

    update the producer kafka

--- kafka-python/src/producer.py
+++ kafka-python/src/producer.py
@@ -1,27 +1,23 @@
-import time 
-import json 
-import random 
+""" 
+Kafka Producer 
+"""
+import json
 from datetime import datetime
-from data_generator import generate_message
 from kafka import KafkaProducer
-# Messages will be serialized as JSON 
+
+
+# Messages will be serialized as JSON
 def serializer(message):
     return json.dumps(message).encode('utf-8')
+
 # Kafka Producer
 producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['kafka:9092'],
     value_serializer=serializer
 )
-if __name__ == '__main__':
-    # Infinite loop - runs until you kill the program
-    while True:
-        # Generate a message
-        dummy_message = generate_message()
-        
-        # Send it to our 'messages' topic
-        print(f'Producing message @ {datetime.now()} | Message = {str(dummy_message)}')
-        producer.send('messages', dummy_message)
-        
-        # Sleep for a random number of seconds
-        time_to_sleep = random.randint(1, 11)
-        time.sleep(time_to_sleep)
+
+# producer function get's call in the get_playlist_from_spotify.py
+def producer_func(message):
+        print(f'Producing message @ {datetime.now()} | Message = {str(message)}')
+        producer.send('messages', message)
+
